2e19b116bbf159cc48739c1230ccae47a03113eb
Rony Armenta <ronyarmenta38@gmail.com>
2025-06-10 10:28:19 -0700

    new paths to the web app

--- flaskblog.py
+++ flaskblog.py
@@ -3,11 +3,17 @@ app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/home")
 def hello():
     return "<h1>Hello World!</h1>"
 
 
-if __name__ == "__main__":
+@app.route("/about")
+def about():
+    return "<h1>About Page, welcome everybody</h1>"
+
+
+if __name__ == '__main__':
     app.run(debug=True)
 # To run this Flask application, you can use the following commands in your terminal:
 
