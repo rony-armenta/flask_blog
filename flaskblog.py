c0767ac47de398b5c6db4c7cc97a87b220f703bb
Rony Armenta <ronyarmenta38@gmail.com>
2025-06-10 13:46:58 -0700

    Add layout file, Bootstrap design using CSS

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,33 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Rony Armenta',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Jun 10 2025'
+    },
+
+    {
+        'author': 'Tony Artemna',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Jun 09 2025'
+    }
+]
+
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template('home.html', title='Home Page')
+    return render_template('home.html', title='Home', posts=posts)
 
 
 @app.route("/about")
 def about():
     # return "<h1>About Page, welcome everybody</h1>"
-    return render_template('about.html', title='About Page')
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
